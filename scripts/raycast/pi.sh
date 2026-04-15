9499f5982ac25b8ac4e8f79ad0eb4d304e789d37
Herb Caudill <herb@devresults.com>
2026-04-15 16:31:59 +0200

    pi.sh: add new script

---
+++ scripts/raycast/pi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Pi
+# @raycast.mode silent
+# @raycast.icon 🤖
+
+osascript -e '
+tell application "iTerm"
+    create window with default profile
+    tell current session of current window
+        write text "cd ~/Code/herbcaudill && cls && pi"
+    end tell
+    tell current window
+        set bounds to {100, 100, 1400, 900}
+    end tell
+end tell'
