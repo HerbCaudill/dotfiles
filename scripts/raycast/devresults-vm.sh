826005362785c3150bdf1489e9532bac3c23efaa
Herb Caudill <herb@devresults.com>
2026-04-21 14:50:34 +0200

    raycast: add DevResults VM command

---
+++ scripts/raycast/devresults-vm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title DevResults VM
+# @raycast.mode fullOutput
+# @raycast.packageName Developer Utils
+
+# Optional parameters:
+# @raycast.icon 💻
+# @raycast.description Start the Windows VM if needed and open C:/Code/DevResults in VS Code Remote-SSH
+
+exec devresults-vm
