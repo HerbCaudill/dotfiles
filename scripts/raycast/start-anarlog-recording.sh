ecab3d352dcc149fc83ccdd11bf62e3473e968eb
Herb Caudill <herb@devresults.com>
2026-06-17 19:50:41 +0200

    Raycast: avoid anarlog bundle identifier lookup

--- scripts/raycast/start-anarlog-recording.sh
+++ scripts/raycast/start-anarlog-recording.sh
@@ -7,22 +7,19 @@
 # @raycast.packageName Meetings
 
 osascript <<'APPLESCRIPT'
-set appId to "com.hyprnote.stable"
-
-tell application id appId to activate
+tell application "anarlog" to activate
 
 tell application "System Events"
   repeat 100 times
-    if exists application process whose bundle identifier is appId then
-      set anarlogProcess to first application process whose bundle identifier is appId
-      if frontmost of anarlogProcess then exit repeat
-      set frontmost of anarlogProcess to true
+    if exists process "anarlog" then
+      tell process "anarlog" to set frontmost to true
+      delay 0.1
+      if frontmost of process "anarlog" then exit repeat
     end if
 
     delay 0.1
   end repeat
 
-  delay 0.2
   keystroke "n" using {command down, shift down}
 end tell
 APPLESCRIPT
