a8dd1ad65dd5204797279d1d2e7fe3347cd98bbe
Herb Caudill <herb@devresults.com>
2026-06-17 19:46:38 +0200

    Raycast: wait for anarlog before recording

--- scripts/raycast/start-anarlog-recording.sh
+++ scripts/raycast/start-anarlog-recording.sh
@@ -8,8 +8,19 @@
 
 osascript <<'APPLESCRIPT'
 tell application "anarlog" to activate
-delay 0.4
+
 tell application "System Events"
+  repeat 100 times
+    if exists process "anarlog" then
+      tell process "anarlog"
+        if frontmost and exists menu bar 1 then exit repeat
+      end tell
+    end if
+
+    delay 0.1
+  end repeat
+
+  tell process "anarlog" to set frontmost to true
   keystroke "n" using {command down, shift down}
 end tell
 APPLESCRIPT
